1145a7c8f195abe59fdc702e095054b7c20f50d4
Malcom Bey <malcombey@schiff.local>
2021-10-18 17:37:35 -0300

    :zap: initial commit, undone work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+export interface Product {
+  cols: number;
+  rows: number;
+  name: string;
+  price: number;
+  discount: number;
+  tax: number;
+  picked: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agnos-challenge';
+  title = 'Agnos Challenge';
+  loading = false;
+  products: Product[] = [
+    {name: 'Coffe', cols: 1, rows: 1, price: 3, discount: 2, tax: 2, picked: false},
+    {name: 'Te', cols: 1, rows: 1, price: 2, discount: 1, tax: 2, picked: false},
+    {name: 'Sandwich', cols: 1, rows: 1, price: 5, discount: 3, tax: 1, picked: false},
+    {name: 'A whole launch', cols: 1, rows: 1, price: 20, discount: 4, tax: 1, picked: false},
+  ];
+
+  // TODO: An array of discount with conditions to dont overlap promotions 
+
+  constructor(private _snackBar: MatSnackBar){
+
+  }
+  
+  async openSnackBar(message: string, amount: number) {
+    message = message + ` $$  ${amount}`;
+    this.loading = true;
+    await new Promise(resolve => setTimeout(() => {
+      this.loading = false;
+      this._snackBar.open(message);
+    }, 2000));
+  }
+
+  pickItem(index: number) {
+    this.products[index].picked = !this.products[index].picked;
+  }
+
+  getItems() {
+    return this.products.filter((product) => product.picked === true);
+  }
+
+  getAmount() {
+    const products = this.products.filter((product) => product.picked === true);
+    let total = 0;
+    products.forEach((element) => {
+        total += element.price;
+    })
+    // TODO: apply tax then discount based on promotions
+    return total;
+  }
+
+  enablePay(){
+    const items = this.getItems();
+    return !(items.length > 0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatProgressSpinnerModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
